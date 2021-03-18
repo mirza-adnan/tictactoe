bae8da77c2c1b4e9b7bbd7b6d5a5e3557247ef0a
mizzadnan <mirza.adnan2205@gmail.com>
2021-03-19 00:33:03 +0600

    added renderBoard function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const SelectionScreen = (function() {
     
-    const playerFactory = function(player, mark, isAi) {
+    const playerFactory = function(player, mark, isAi, turn) {
         return {
             player,
             mark,
-            isAi
+            isAi,
+            turn
         }
     }
 
     // creating the players
-    const player1 = playerFactory("Player 1", "X", false);
-    const player2 = playerFactory("Player 2", "O", false);
+    const player1 = playerFactory("Player 1", "X", false, true);
+    const player2 = playerFactory("Player 2", "O", false, true);
     
+    // closing the selection screen after choosing opponent
     const human = document.querySelector(".human");
     const computer = document.querySelector(".computer");
 
@@ -20,7 +22,6 @@ const SelectionScreen = (function() {
         player2.isAi = true;
         closeSelectionScreen();
     })
-
 
     function closeSelectionScreen() {
         const selectionScreen = document.querySelector(".selection-screen");
@@ -35,10 +36,25 @@ const SelectionScreen = (function() {
 })()
 
 const Gameboard = (function() {
-    let board = ["", "", "", "", "", "", "", "", ""];
+    
+    let board = ["X", "O", "X", "", "", "", "", "", ""];  //array to hold the X and O's
+    
+    const squares = document.querySelectorAll(".square");
+    console.log(squares)
+
+    function renderBoard() {
+        for(let i=0; i<9; i++) {
+            squares[i].textContent = board[i];
+        }
+    }
+    return {
+        renderBoard,
+    }
+
 })();
 
 const Controller = (function() {
 
 })()
 
+Gameboard.renderBoard();
