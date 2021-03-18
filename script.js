cd1297655f2775823d2115e865d42ee956b3c79b
mizzadnan <mirza.adnan2205@gmail.com>
2021-03-19 01:01:21 +0600

    added function for x and o to appear based on the players turn

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const SelectionScreen = (function() {
 
     // creating the players
     const player1 = playerFactory("Player 1", "X", false, true);
-    const player2 = playerFactory("Player 2", "O", false, true);
+    const player2 = playerFactory("Player 2", "O", false, false);
     
     // closing the selection screen after choosing opponent
     const human = document.querySelector(".human");
@@ -33,24 +33,56 @@ const SelectionScreen = (function() {
         selectionScreen.style.opacity = "0";
     }
 
+    return {
+        player1,
+        player2
+    }
+
 })()
 
 const Gameboard = (function() {
     
-    let board = ["X", "O", "X", "", "", "", "", "", ""];  //array to hold the X and O's
+    let board = ["", "", "", "", "", "", "", "", ""];  //array to hold the X and O's
     
     const squares = document.querySelectorAll(".square");
-    console.log(squares)
+
+    squares.forEach(square => {
+        square.addEventListener("click", updateBoard);
+    })
 
     function renderBoard() {
         for(let i=0; i<9; i++) {
             squares[i].textContent = board[i];
         }
     }
+
+    function updateBoard() {
+        const index = Number(this.getAttribute("data-index"));
+        if (SelectionScreen.player1.turn) {
+            board[index] = SelectionScreen.player1.mark;
+        }
+        if (SelectionScreen.player2.turn) {
+            board[index] = SelectionScreen.player2.mark;
+        }
+        renderBoard();
+        changeOpacity(this);
+        changeTurns()
+    }
+
+    // toggles the turn of each player
+    function changeTurns() {
+        SelectionScreen.player1.turn = !SelectionScreen.player1.turn;
+        SelectionScreen.player2.turn = !SelectionScreen.player2.turn;
+    }
+
+    function changeOpacity(element) {
+        element.style.color = "rgba(232, 90, 79, 1)"  // increasing the alpha value from 0 to 1 to make it appear slowly
+    }
+
+
     return {
         renderBoard,
     }
-
 })();
 
 const Controller = (function() {
